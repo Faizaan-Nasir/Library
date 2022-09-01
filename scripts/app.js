a642ec9665a0127bb8c69cef02dfccd5247f3e9c
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-09-02 03:25:22 +0530

    added a penalty advisor system

--- scripts/app.js
+++ scripts/app.js
@@ -15,6 +15,22 @@ function request() {
     document.getElementById("book_issued").innerHTML = localStorage.getItem('book_issued' + document.getElementById("sno").value)
     document.getElementById("date_of_issue").innerHTML = localStorage.getItem('date_of_issue' + document.getElementById("sno").value)
     document.getElementById("date_of_return").innerHTML = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
+    DOR = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
+    yyyyr = `${DOR.charAt(6)}` + `${DOR.charAt(7)}` + `${DOR.charAt(8)}` + `${DOR.charAt(9)}`
+    mmr = `${DOR.charAt(3)}` + `${DOR.charAt(4)}`
+    ddr = `${DOR.charAt(0)}` + `${DOR.charAt(1)}`
+    if (yyyy > yyyyr) {
+        alert("you have a fine")
+    }
+    else if (mm > mmr) {
+        alert("you have a fine")
+    }
+    else if (dd > ddr) {
+        alert("you have a fine")
+    }
+    else {
+
+    }
 }
 function cleardata() {
     localStorage.removeItem('book_issued' + document.getElementById("sno").value)
