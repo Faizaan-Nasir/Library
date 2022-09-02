2c4ed2a31eab8f2d7fe2695ea2691223f4dd0dbc
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-09-02 17:52:12 +0530

    added a search bar

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,23 @@
-// fetching today's date
+// fetching today's date:
+
+function search() {
+    let s = document.getElementById("search").value
+    x = `${Number(s)}`
+    if (x == 'NaN') {
+        var sno = localStorage.getItem('sno' + s)
+        if (sno) {
+            document.getElementById("sno").innerHTML = sno;
+            request()
+        }
+        else {
+            alert("such book does not exist in database")
+        }
+    }
+    else {
+        document.getElementById("sno").innerHTML = s;
+        request()
+    }
+}
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
@@ -11,17 +30,19 @@ function displaydate() {
 function save() {
     localStorage.setItem('name' + document.getElementById("sno").value, document.getElementById("name").value)
     localStorage.setItem('book_issued' + document.getElementById("sno").value, document.getElementById("book_issued").value)
+    localStorage.setItem('sno' + document.getElementById("book_issued").value, document.getElementById("sno").value)
     localStorage.setItem('date_of_issue' + document.getElementById("sno").value, dd + '/' + mm + '/' + yyyy)
     localStorage.setItem('date_of_return' + document.getElementById("sno").value, document.getElementById("date_of_return").value)
     document.getElementById("verified").innerHTML = "Data Submitted Sucessfully"
     setTimeout(function reload() { location.reload() }, 3000)
 }
-// requests for info and decides whether the student has a fine or not
+// requests for info and decides whether the student has a fine or not:
 function request() {
     document.getElementById("name").innerHTML = localStorage.getItem('name' + document.getElementById("sno").value)
     document.getElementById("book_issued").innerHTML = localStorage.getItem('book_issued' + document.getElementById("sno").value)
     document.getElementById("date_of_issue").innerHTML = localStorage.getItem('date_of_issue' + document.getElementById("sno").value)
     document.getElementById("date_of_return").innerHTML = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
+    // here starts the code for penalty:
     var DOR = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
     var yyyyr = `${DOR.charAt(6)}` + `${DOR.charAt(7)}` + `${DOR.charAt(8)}` + `${DOR.charAt(9)}`
     var mmr = `${DOR.charAt(3)}` + `${DOR.charAt(4)}`
@@ -44,8 +65,10 @@ function request() {
 }
 // clears data:
 function cleardata() {
-    localStorage.removeItem('book_issued' + document.getElementById("sno").value)
-    localStorage.removeItem('date_of_issue' + document.getElementById("sno").value)
-    localStorage.removeItem('date_of_return' + document.getElementById("sno").value)
-    request()
+    if (confirm("Are you sure you want to clear data?") == true) {
+        localStorage.removeItem('book_issued' + document.getElementById("sno").value)
+        localStorage.removeItem('date_of_issue' + document.getElementById("sno").value)
+        localStorage.removeItem('date_of_return' + document.getElementById("sno").value)
+        request()
+    }
 }
