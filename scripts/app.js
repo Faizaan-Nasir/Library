3b7b32093cbc11f1a3c28acaa22e9f21df6c8c15
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-09-02 14:38:02 +0530

    THE PENALTY SYSTEM WORKS

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,7 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yyyy = today.getFullYear();
 var date = dd + '/' + mm + '/' + yyyy
+var date_format = new Date(yyyy + '-' + mm + '-' + dd)
 function displaydate() {
     document.getElementById("date_of_issue").innerHTML = date
 }
@@ -21,18 +22,21 @@ function request() {
     document.getElementById("book_issued").innerHTML = localStorage.getItem('book_issued' + document.getElementById("sno").value)
     document.getElementById("date_of_issue").innerHTML = localStorage.getItem('date_of_issue' + document.getElementById("sno").value)
     document.getElementById("date_of_return").innerHTML = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
-    DOR = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
-    yyyyr = `${DOR.charAt(6)}` + `${DOR.charAt(7)}` + `${DOR.charAt(8)}` + `${DOR.charAt(9)}`
-    mmr = `${DOR.charAt(3)}` + `${DOR.charAt(4)}`
-    ddr = `${DOR.charAt(0)}` + `${DOR.charAt(1)}`
+    var DOR = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
+    var yyyyr = `${DOR.charAt(6)}` + `${DOR.charAt(7)}` + `${DOR.charAt(8)}` + `${DOR.charAt(9)}`
+    var mmr = `${DOR.charAt(3)}` + `${DOR.charAt(4)}`
+    var ddr = `${DOR.charAt(0)}` + `${DOR.charAt(1)}`
+    var DOR_format = new Date(yyyyr + '-' + mmr + '-' + ddr)
+    var dif = (date_format - DOR_format) / (1000 * 60 * 60 * 24)
+    var penalty = dif * 0.5
     if (yyyy > yyyyr) {
-        document.getElementById("penalty").innerHTML = 'The Student has a Fine'
+        document.getElementById("penalty").innerHTML = 'The student has a fine of AED ' + penalty
     }
     else if (mm > mmr) {
-        document.getElementById("penalty").innerHTML = 'The Student has a Fine'
+        document.getElementById("penalty").innerHTML = 'The student has a fine of AED ' + penalty
     }
     else if (dd > ddr) {
-        document.getElementById("penalty").innerHTML = 'The Student has a Fine'
+        document.getElementById("penalty").innerHTML = 'The student has a fine AED ' + penalty
     }
     else {
 
