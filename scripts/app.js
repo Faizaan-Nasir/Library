433ff749f356a066cdacc5b92d8042db31d84155
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-09-11 19:52:03 +0530

    connected to a db

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,28 @@
 // filter for search bar
 function search() {
     let s = document.getElementById("search").value
-    x = `${Number(s)}`
-    if (x == 'NaN') {
-        var sno = localStorage.getItem('sno' + s)
-        if (sno) {
-            document.getElementById("sno").innerHTML = sno;
-            request()
-        }
-        else {
-            alert("Such a book is not issued by any student!")
+
+    const api_url = "https://sheetdb.io/api/v1/qpwk686wb4g5z"
+    async function getData() {
+        const response = await fetch(api_url)
+        const data = await response.json()
+        console.log(data)
+        let x = data.length
+        for (i = 0; i < x; i++) {
+            let lib = data[i]
+            if (lib['sno'] === `${s}`) {
+                const a = i
+                console.log(a)
+                document.getElementById("sno").innerHTML = lib['sno']
+                document.getElementById("name").innerHTML = lib['name']
+                document.getElementById("book_issued").innerHTML = lib['book_issued']
+                document.getElementById("date_of_issue").innerHTML = lib['date_of_issue']
+                document.getElementById("date_of_return").innerHTML = lib['date_of_return']
+            }
         }
     }
-    else {
-        document.getElementById("sno").innerHTML = s;
-        request()
-    }
+    getData()
+
 }
 // fetching today's date:
 var today = new Date();
@@ -27,35 +34,17 @@ var date_format = new Date(yyyy + '-' + mm + '-' + dd)
 function displaydate() {
     document.getElementById("date_of_issue").innerHTML = date
 }
-function save() {
-    // checking if sno is a number
-    if (`${Number(document.getElementById("sno").value)}` == 'NaN') {
-        alert("Student ID Number field must be a number. Cannot run the function.")
-    }
-    else {
-        // checking if any field is blank
-        if (`${document.getElementById("name").value}` == '' || `${document.getElementById("book_issued").value}` == '' || `${document.getElementById("sno").value}` == '' || `${document.getElementById("date_of_return").value}` == '') {
-            alert("Necessary field(s) is left blank, please fill out the field(s) before submitting.")
-        }
-        else {
-            // saving all data
-            localStorage.setItem('name' + document.getElementById("sno").value, document.getElementById("name").value)
-            localStorage.setItem('book_issued' + document.getElementById("sno").value, document.getElementById("book_issued").value)
-            localStorage.setItem('sno' + document.getElementById("book_issued").value, document.getElementById("sno").value)
-            localStorage.setItem('date_of_issue' + document.getElementById("sno").value, dd + '/' + mm + '/' + yyyy)
-            localStorage.setItem('date_of_return' + document.getElementById("sno").value, document.getElementById("date_of_return").value)
-            document.getElementById("verified").innerHTML = "Data Submitted Sucessfully"
-            setTimeout(function reload() { location.reload() }, 3000)
-        }
-    }
-}
+
 // requests for info and decides whether the student has a fine or not:
 function request() {
-    document.getElementById("penalty").innerHTML = ""
-    document.getElementById("name").innerHTML = localStorage.getItem('name' + document.getElementById("sno").value)
-    document.getElementById("book_issued").innerHTML = localStorage.getItem('book_issued' + document.getElementById("sno").value)
-    document.getElementById("date_of_issue").innerHTML = localStorage.getItem('date_of_issue' + document.getElementById("sno").value)
-    document.getElementById("date_of_return").innerHTML = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
+    const api_url = "https://sheetdb.io/api/v1/qpwk686wb4g5z"
+    async function getData() {
+        const response = await fetch(api_url)
+        const data = await response.json()
+        const { sno, name, book_issued, date_of_return, date_of_issue } = data
+    }
+    getData()
+
     // here starts the code for penalty:
     var DOR = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
     var yyyyr = `${DOR.charAt(6)}` + `${DOR.charAt(7)}` + `${DOR.charAt(8)}` + `${DOR.charAt(9)}`
