85ed9ec8e5333a3920303bccd39b6bd068be3b6d
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-09-12 22:51:09 +0530

    some more changes attempting to refine clear

--- scripts/app.js
+++ scripts/app.js
@@ -25,6 +25,25 @@ function search() {
                 document.getElementById("date_of_issue").innerHTML = lib['date_of_issue']
                 document.getElementById("date_of_return").innerHTML = lib['date_of_return']
             }
+            else if (lib['name'] === `${s}`) {
+                const a = i
+                console.log(a)
+                document.getElementById("sno").innerHTML = lib['sno']
+                document.getElementById("name").innerHTML = lib['name']
+                document.getElementById("book_issued").innerHTML = lib['book_issued']
+                document.getElementById("date_of_issue").innerHTML = lib['date_of_issue']
+                document.getElementById("date_of_return").innerHTML = lib['date_of_return']
+            }
+            else if (lib['book_issued'] === `${s}`) {
+                const a = i
+                console.log(a)
+                document.getElementById("sno").innerHTML = lib['sno']
+                document.getElementById("name").innerHTML = lib['name']
+                document.getElementById("book_issued").innerHTML = lib['book_issued']
+                document.getElementById("date_of_issue").innerHTML = lib['date_of_issue']
+                document.getElementById("date_of_return").innerHTML = lib['date_of_return']
+            }
+
         }
     }
 
@@ -40,32 +59,4 @@ var date = dd + '/' + mm + '/' + yyyy
 var date_format = new Date(yyyy + '-' + mm + '-' + dd)
 function displaydate() {
     document.getElementById("date_of_issue").innerHTML = date
-}
-
-// here starts the code for penalty:
-// var DOR = localStorage.getItem('date_of_return' + document.getElementById("sno").value)
-// var yyyyr = `${DOR.charAt(6)}` + `${DOR.charAt(7)}` + `${DOR.charAt(8)}` + `${DOR.charAt(9)}`
-// var mmr = `${DOR.charAt(3)}` + `${DOR.charAt(4)}`
-// var ddr = `${DOR.charAt(0)}` + `${DOR.charAt(1)}`
-// var DOR_format = new Date(yyyyr + '-' + mmr + '-' + ddr)
-// var dif = (date_format - DOR_format) / (1000 * 60 * 60 * 24)
-// var penalty = dif * 0.5
-// if (yyyy > yyyyr) {
-//     document.getElementById("penalty").innerHTML = 'The student has a fine of AED ' + penalty
-// }
-// else if (mm > mmr) {
-//     document.getElementById("penalty").innerHTML = 'The student has a fine of AED ' + penalty
-// }
-// else if (dd > ddr) {
-//     document.getElementById("penalty").innerHTML = 'The student has a fine AED ' + penalty
-// }
-
-// clears data:
-function cleardata() {
-    if (confirm("Are you sure you want to clear data?") == true) {
-        localStorage.removeItem('book_issued' + document.getElementById("sno").value)
-        localStorage.removeItem('date_of_issue' + document.getElementById("sno").value)
-        localStorage.removeItem('date_of_return' + document.getElementById("sno").value)
-        request()
-    }
 }
